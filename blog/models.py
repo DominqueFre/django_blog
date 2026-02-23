07e39f80a5d6d1466da09c2e2f15ed04eb63481c
DominiqueFre <dominiquereade@gmail.com>
2026-02-23 17:44:22 +0000

    add updated on

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     content = models.TextField(blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
