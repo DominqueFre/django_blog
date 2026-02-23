58591af355e6d8de1af88101ec745e6ee9b7386d
DominiqueFre <dominiquereade@gmail.com>
2026-02-23 10:29:28 +0000

    allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    '.localhost',
+    '::1',
+    '127.0.0.1',
+]
 
 
 # Application definition
