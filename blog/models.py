3088f3e11c0af2ea4ad07f220d62e3e157b38bfa
DominiqueFre <dominiquereade@gmail.com>
2026-02-24 12:17:41 +0000

    add comments

--- blog/models.py
+++ blog/models.py
@@ -23,3 +23,21 @@ class Post(models.Model):
         ordering = ['-created_on'],
         verbose_name = 'Post',
         verbose_name_plural = 'Posts'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments', blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='commenter', blank=False)
+    body = models.TextField(blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.author)
