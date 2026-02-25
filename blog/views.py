a707e511f8827c889c9fce03f3d88c9676be6d50
DominiqueFre <dominiquereade@gmail.com>
2026-02-25 16:24:06 +0000

    add css

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render #,redirect
 from django.views import generic
 from .models import Post
 
@@ -9,3 +9,38 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = 'blog/index.html'
     paginate_by = 6
+
+
+# class PostDetail(generic.DetailView):
+#     model = Post
+#     template_name = 'blog/post_detail.html'
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs) 
+#         context['post_list'] = Post.objects.filter(status=1).order_by('-created_on')[:5]
+#         return context
+
+# def post_detail(request, slug):
+#     post = get_object_or_404(Post, slug=slug)
+#     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+ # def delete_post(request, slug):
+#     post = get_object_or_404(Post, slug=slug) 
+#    if request.method == 'POST':
+#       post.delete()
+#      return redirect('home')
+#   return render(request, 'blog/post_confirm_delete.html', {'post': post})
+#   
+# def post_detail(request, slug):
+#     post = get_object_or_404(Post, slug=slug)
+#    if request.method == 'POST':
+#       post.delete()
+#     return redirect('home')
+#   return render(request, 'blog/post_detail.html', {'post': post})
+# 
+
+# 
+# def post_detail(request, slug):
+#   post = get_object_or_404(Post, slug=slug)
+# if request.method == 'POST':
+#   post.delete()   
